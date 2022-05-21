74570e2551b8fc29d4418f95821205d0ca7b8641
Sven A. Schmidt <sas@finestructure.co>
2022-05-21 05:52:47 +0200

    Add basic cache-hit command

--- Package.swift
+++ Package.swift
@@ -6,12 +6,14 @@ let package = Package(
     name: "swift-pg-extras",
     platforms: [.macOS(.v12)],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.1.2")
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.1.2"),
+        .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.10.0"),
     ],
     targets: [
         .executableTarget(name: "pg-extras", dependencies: ["PGExtras"]),
         .target(name: "PGExtras", dependencies: [
-            .product(name: "ArgumentParser", package: "swift-argument-parser")
+            .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            .product(name: "PostgresNIO", package: "postgres-nio"),
         ]),
         .testTarget(name: "pg-extrasTests", dependencies: ["pg-extras"]),
     ]
--- Sources/PGExtras/Commands/CacheHit.swift
+++ Sources/PGExtras/Commands/CacheHit.swift
@@ -1,9 +1,43 @@
 import ArgumentParser
+import PostgresNIO
+import Foundation
 
 
 struct CacheHit: AsyncParsableCommand {
     func run() async throws {
         print("cache hit")
+
+        let config = PostgresConnection.Configuration(
+            connection: .init(
+                host: "localhost",
+                port: 6432
+            ),
+            authentication: .init(
+                username: "spi_dev",
+                database: "spi_dev",
+                password: "xxx"
+            ),
+            tls: .disable
+        )
+
+        let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+        defer { try? eventLoopGroup.syncShutdownGracefully() }
+
+        let logger = Logger(label: "pg-extras")
+
+        let conn = try await PostgresConnection.connect(on: eventLoopGroup.next(),
+                                                        configuration: config,
+                                                        id: 1,
+                                                        logger: logger)
+
+        do {
+            let rows = try await conn.query(.init(stringLiteral: Self.sql), logger: logger)
+            for try await (name, ratio) in rows.decode((String, Decimal?).self, context: .default) {
+                print(name, ratio)
+            }
+        }
+
+        try await conn.close()
     }
 }
 
@@ -18,7 +52,7 @@ extension CacheHit {
         SELECT
         'table hit rate' AS name,
         sum(heap_blks_hit) / nullif(sum(heap_blks_hit) + sum(heap_blks_read),0) AS ratio
-        FROM pg_statio_user_tables;`
+        FROM pg_statio_user_tables
         """
 
 }
