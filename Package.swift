33c492626ddb1619666a961ef7d2f176af038352
Sven A. Schmidt <sas@finestructure.co>
2022-05-23 10:34:58 +0200

    Revert "Swift 5.5, macOS v11"
    This reverts commit 88407b8c4d01df6666e68d5ea1b261346b37585f.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.6
 
 import PackageDescription
 
 let package = Package(
     name: "PGExtras",
-    platforms: [.macOS(.v11)],
+    platforms: [.macOS(.v12)],
     products: [
         .executable(name: "pg-extras", targets: ["pg-extras"]),
         .library(name: "PGExtras", targets: ["PGExtras"])
